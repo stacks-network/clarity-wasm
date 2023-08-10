560d82c079b8fd58e26f659445eb82e601d3d59f
Brice Dobry <brice@hiro.so>
2023-08-10 13:59:27 -0400

    docs: add documentation for `ASTVisitor`

--- src/ast_visitor.rs
+++ src/ast_visitor.rs
@@ -25,6 +25,24 @@ lazy_static! {
     static ref DEFAULT_EXPR: SymbolicExpression = SymbolicExpression::atom(DEFAULT_NAME.clone());
 }
 
+/// The ASTVisitor trait specifies the interfaces needed to build a visitor
+/// to walk a Clarity abstract syntax tree (AST). All methods have default
+/// implementations so that any left undefined in an implementation will
+/// perform a standard walk through the AST, ensuring that all sub-expressions
+/// are visited as appropriate. If a `traverse_*` method is implemented, then
+/// the implementation is responsible for traversing the sub-expressions.
+///
+/// Traversal is post-order, so the sub-expressions are visited before the
+/// parent is visited. To walk through an example, if we visit the AST for the
+/// Clarity expression `(+ a 1)`, we would hit the following methods in order:
+/// 1. `traverse_expr`: `(+ a 1)`
+/// 2. `traverse_list`: `(+ a 1)`
+/// 3. `traverse_arithmetic`: `(+ a 1)`
+/// 4. `traverse_expr`: `a`
+/// 5. `visit_atom`: `a`
+/// 6. `traverse_expr`: `1`
+/// 7. `visit_literal_value`: `1`
+/// 8. `visit_arithmetic`: `(+ a 1)`
 pub trait ASTVisitor<'a> {
     fn traverse_expr<'b>(
         &mut self,
