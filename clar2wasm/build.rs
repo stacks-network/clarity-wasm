acf159bd3c5e95b0e82eacd03b22740f75eed45d
Anthony Caccia <anthonycaccia24@gmail.com>
2023-08-15 16:29:22 +0200

    build(clar2wasm): generate standard.wasm at each change from standard.wat

---
+++ clar2wasm/build.rs
@@ -0,0 +1,13 @@
+/// Generate the standard library as a Wasm binary from the WAT source.
+fn main() {
+    match wat::parse_file("src/standard/standard.wat") {
+        Ok(binary) => {
+            std::fs::write("src/standard/standard.wasm", binary)
+                .expect("Failed to write standard library");
+        }
+        Err(error) => {
+            panic!("Failed to parse standard library: {error}");
+        }
+    };
+    println!("cargo:rerun-if-changed=src/standard/standard.wat");
+}
