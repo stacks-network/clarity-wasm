f3a55e1125f45090aca100a2973d3cd094717c0d
Brice Dobry <brice@hiro.so>
2023-08-16 01:26:13 -0400

    feat: finish support for `var-set`

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -519,4 +519,59 @@ mod tests {
         }
         global_context.commit().unwrap();
     }
+
+    #[test]
+    fn var_set() {
+        let contract_id = QualifiedContractIdentifier::new(
+            StandardPrincipalData::transient(),
+            ContractName::from("var-set"),
+        );
+        let mut datastore = Datastore::new();
+        let constants = StacksConstants {
+            burn_start_height: 0,
+            pox_prepare_length: 0,
+            pox_reward_cycle_length: 0,
+            pox_rejection_fraction: 0,
+            epoch_21_start_height: 0,
+        };
+        let burn_datastore = BurnDatastore::new(constants);
+        let mut conn = ClarityDatabase::new(&mut datastore, &burn_datastore, &burn_datastore);
+        conn.begin();
+        conn.set_clarity_epoch_version(StacksEpochId::Epoch24);
+        conn.commit();
+        let cost_tracker = LimitedCostTracker::new_free();
+        let mut global_context = GlobalContext::new(
+            false,
+            CHAIN_ID_TESTNET,
+            conn,
+            cost_tracker,
+            StacksEpochId::Epoch24,
+        );
+        let mut contract_context =
+            ContractContext::new(contract_id.clone(), ClarityVersion::Clarity2);
+
+        global_context.begin();
+        {
+            let mut helper =
+                WasmtimeHelper::new(contract_id, &mut global_context, &mut contract_context);
+
+            if let ClarityWasmResult::Response {
+                indicator,
+                ok_value,
+                err_value,
+            } = helper.call_public_function("simple", &[])
+            {
+                assert_eq!(indicator, 1);
+                assert!(ok_value.is_some());
+                assert!(err_value.is_none());
+                if let ClarityWasmResult::Int { high, low } = *ok_value.unwrap() {
+                    assert_eq!(high, 0x123);
+                    assert_eq!(low, 0x456);
+                }
+            } else {
+                panic!("Unexpected result received from WASM function call.");
+            }
+        }
+        global_context.commit().unwrap();
+    }
 }
