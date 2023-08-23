8dc660c1cf450904399f6fbe263232170d6152df
Anthony Caccia <anthonycaccia24@gmail.com>
2023-08-23 20:02:13 +0200

    test(clar2wasm): POC of proptest for add-uint

--- clar2wasm/src/standard/tests.rs
+++ clar2wasm/src/standard/tests.rs
@@ -1,3 +1,5 @@
+use proptest::prelude::*;
+
 use std::borrow::BorrowMut;
 use wasmtime::{Caller, Engine, Instance, Linker, Module, Store, Val};
 
@@ -58,11 +60,16 @@ fn load_stdlib() -> Result<(Instance, Store<()>), wasmtime::Error> {
     Ok((instance, store))
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 struct PropInt(u128);
 
 impl PropInt {
     const fn new(n: u128) -> Self {
         Self(n)
+    }
+
+    const fn from_wasm(high: i64, low: i64) -> Self {
+        Self(((high as u64) as u128) << 64 | ((low as u64) as u128))
     }
 
     const fn signed(&self) -> i128 {
@@ -79,6 +86,34 @@ impl PropInt {
 
     const fn low(&self) -> i64 {
         self.0 as i64
+    }
+}
+
+prop_compose! {
+    fn int128()(n in any::<u128>()) -> PropInt {
+        PropInt::new(n)
+    }
+}
+
+proptest! {
+    #[test]
+    fn prop_add_uint(n in int128(), m in int128()) {
+        let (instance, mut store) = load_stdlib().unwrap();
+        let add = instance.get_func(store.borrow_mut(), "add-uint").unwrap();
+        let mut sum = [Val::I64(0), Val::I64(0)];
+        let res = add.call(
+            store.borrow_mut(),
+            &[n.high().into(), n.low().into(), m.high().into(), m.low().into()],
+            &mut sum,
+        );
+        match n.unsigned().checked_add(m.unsigned()) {
+            Some(rust_result) => {
+                res.expect("call to add-uint failed");
+                let wasm_result = PropInt::from_wasm(sum[0].i64().unwrap(), sum[1].i64().unwrap());
+                prop_assert_eq!(rust_result, wasm_result.unsigned());
+            }
+            None => {res.expect_err("expected overflow");}
+        }
     }
 }
 
