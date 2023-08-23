6001faec04c99f12c13bbbc6633ba125311ec813
Anthony Caccia <anthonycaccia24@gmail.com>
2023-08-23 22:04:25 +0200

    test(clar2wasm): add prop tests for mul-uint and mul-int

--- clar2wasm/tests/standard/property_tests.rs
+++ clar2wasm/tests/standard/property_tests.rs
@@ -83,7 +83,7 @@ fn prop_add_int() {
         );
         match n.signed().checked_add(m.signed()) {
             Some(rust_result) => {
-                call.expect("call to add-uint failed");
+                call.expect("call to add-int failed");
                 let wasm_result = PropInt::from_wasm(res[0].i64().unwrap(), res[1].i64().unwrap());
                 prop_assert_eq!(rust_result, wasm_result.signed());
             }
@@ -109,7 +109,7 @@ fn prop_sub_uint() {
         );
         match n.unsigned().checked_sub(m.unsigned()) {
             Some(rust_result) => {
-                call.expect("call to add-uint failed");
+                call.expect("call to sub-uint failed");
                 let wasm_result = PropInt::from_wasm(res[0].i64().unwrap(), res[1].i64().unwrap());
                 prop_assert_eq!(rust_result, wasm_result.unsigned());
             }
@@ -135,11 +135,63 @@ fn prop_sub_int() {
         );
         match n.signed().checked_sub(m.signed()) {
             Some(rust_result) => {
-                call.expect("call to add-uint failed");
+                call.expect("call to sub-int failed");
                 let wasm_result = PropInt::from_wasm(res[0].i64().unwrap(), res[1].i64().unwrap());
                 prop_assert_eq!(rust_result, wasm_result.signed());
             }
             None => { call.expect_err("expected underflow"); }
+        }
+    })
+}
+
+#[test]
+fn prop_mul_uint() {
+    let (instance, store) = load_stdlib().unwrap();
+    let store = RefCell::new(store);
+    let mul = instance
+        .get_func(store.borrow_mut().deref_mut(), "mul-uint")
+        .unwrap();
+
+    proptest!(|(n in int128(), m in int128())| {
+        let mut res = [Val::I64(0), Val::I64(0)];
+        let call = mul.call(
+            store.borrow_mut().deref_mut(),
+            &[n.high().into(), n.low().into(), m.high().into(), m.low().into()],
+            &mut res,
+        );
+        match n.unsigned().checked_mul(m.unsigned()) {
+            Some(rust_result) => {
+                call.expect("call to mul-int failed");
+                let wasm_result = PropInt::from_wasm(res[0].i64().unwrap(), res[1].i64().unwrap());
+                prop_assert_eq!(rust_result, wasm_result.unsigned());
+            }
+            None => { call.expect_err("expected overrflow"); }
+        }
+    })
+}
+
+#[test]
+fn prop_mul_int() {
+    let (instance, store) = load_stdlib().unwrap();
+    let store = RefCell::new(store);
+    let mul = instance
+        .get_func(store.borrow_mut().deref_mut(), "mul-int")
+        .unwrap();
+
+    proptest!(|(n in int128(), m in int128())| {
+        let mut res = [Val::I64(0), Val::I64(0)];
+        let call = mul.call(
+            store.borrow_mut().deref_mut(),
+            &[n.high().into(), n.low().into(), m.high().into(), m.low().into()],
+            &mut res,
+        );
+        match n.signed().checked_mul(m.signed()) {
+            Some(rust_result) => {
+                call.expect("call to mul-int failed");
+                let wasm_result = PropInt::from_wasm(res[0].i64().unwrap(), res[1].i64().unwrap());
+                prop_assert_eq!(rust_result, wasm_result.signed());
+            }
+            None => { call.expect_err("expected overflow"); }
         }
     })
 }
