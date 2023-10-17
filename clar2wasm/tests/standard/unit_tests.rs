c5c6d0a6a00b235804f99874ff461c393296b4cc
Anthony Caccia <anthony@bitcoinl2labs.com>
2023-10-17 18:36:57 +0200

    test(clar2wasm): add a test for hash160-int

--- clar2wasm/tests/standard/unit_tests.rs
+++ clar2wasm/tests/standard/unit_tests.rs
@@ -2531,3 +2531,45 @@ fn hash160_buf() {
     let expected_result = Vec::from_hex("d6f2b43388048a339abd861be2babd817e3717cd").unwrap();
     assert_eq!(&buffer, &expected_result);
 }
+
+#[test]
+fn hash160_int() {
+    let (instance, mut store) = load_stdlib().unwrap();
+    let memory = instance
+        .get_memory(&mut store, "memory")
+        .expect("Could not find memory");
+
+    let sha256 = instance.get_func(&mut store, "hash160-int").unwrap();
+    let mut result = [Val::I32(0), Val::I32(0)];
+
+    // This algo needs space on the stack,
+    // we move the initial value of $stack-pointer
+    // to a random one where it wouldn't matter
+    let stack_pointer = instance.get_global(&mut store, "stack-pointer").unwrap();
+    stack_pointer.set(&mut store, Val::I32(1500)).unwrap();
+
+    // The offset where the result hash will be written to
+    let res_offset = 3000i32;
+
+    // Test on 0xfeedc0dedeadbeefcafed00dcafebabe
+    sha256
+        .call(
+            &mut store,
+            &[
+                Val::I64(0xcafed00dcafebabe_u64 as i64),
+                Val::I64(0xfeedc0dedeadbeef_u64 as i64),
+                res_offset.into(),
+            ],
+            &mut result,
+        )
+        .expect("call to hash160-int failed");
+    assert_eq!(result[0].unwrap_i32(), res_offset);
+    assert_eq!(result[1].unwrap_i32(), 20);
+
+    let mut buffer = vec![0u8; result[1].unwrap_i32() as usize];
+    memory
+        .read(&mut store, result[0].unwrap_i32() as usize, &mut buffer)
+        .expect("could not read resulting hash from memory");
+    let expected_result = Vec::from_hex("aeae89e821d429940dff0d3412377815dae9ab07").unwrap();
+    assert_eq!(&buffer, &expected_result);
+}
