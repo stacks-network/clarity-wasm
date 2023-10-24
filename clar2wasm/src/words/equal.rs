9a4acc5ab8a509272e031ac934d4f444d20810b6
Chris Guimaraes <cguimaraes@hiro.so>
2023-10-24 18:02:46 +0100

    feat: fix equal implementation

--- clar2wasm/src/words/equal.rs
+++ clar2wasm/src/words/equal.rs
@@ -19,51 +19,25 @@ impl Word for Equal {
         _expr: &SymbolicExpression,
         args: &[SymbolicExpression],
     ) -> Result<(), GeneratorError> {
-        let name = self.name();
-
-        generator.traverse_args(builder, args)?;
-
-        let ty = generator
-            .get_expr_type(&args[0])
-            .expect("comparison operands must be typed");
-
-        let type_suffix = match ty {
-            TypeSignature::IntType => "int",
-            TypeSignature::UIntType => "uint",
-            TypeSignature::SequenceType(SequenceSubtype::StringType(StringSubtype::ASCII(_))) => {
-                "string-ascii"
-            }
-            TypeSignature::SequenceType(SequenceSubtype::StringType(StringSubtype::UTF8(_))) => {
-                "string-utf8"
-            }
-            TypeSignature::SequenceType(SequenceSubtype::BufferType(_)) => "buffer",
-            _ => {
-                return Err(GeneratorError::InternalError(
-                    "invalid type for comparison".to_string(),
-                ))
-            }
-        };
-
-        let func = generator
-            .module
-            .funcs
-            .by_name(&format!("{name}-{type_suffix}"))
-            .unwrap_or_else(|| panic!("function not found: {name}-{type_suffix}"));
-
         let mut results = Vec::new();
-        generator.traverse_expr(builder, &args[0])?;
-        generator.traverse_expr(builder, &args[1])?;
-        builder.call(func);
+
+        // Traverse the first two values, leaving them on the stack
+        let first_op = args.get_expr(0)?;
+        generator.traverse_expr(builder, first_op)?;
+        let sec_op = args.get_expr(1)?;
+        generator.traverse_expr(builder, sec_op)?;
+
+        builder.call(generator.func_by_name("is-eq-int"));
 
         let result = generator.module.locals.add(ValType::I32);
         builder.local_set(result);
         results.push(result);
 
-        // Push operands, starting from 3rd, onto the stack
+         // Push operands, starting from 3rd, onto the stack
         for operand in args.iter().skip(2) {
-            generator.traverse_expr(builder, &args[0])?;
+            generator.traverse_expr(builder, first_op)?;
             generator.traverse_expr(builder, operand)?;
-            builder.call(func);
+            builder.call(generator.func_by_name("is-eq-int"));
 
             let result = generator.module.locals.add(ValType::I32);
             builder.local_set(result);
@@ -72,9 +46,7 @@ impl Word for Equal {
 
         for result in &results {
             println!("{:?}", generator.module.locals.get(*result));
-
         }
-
 
         builder.local_get(result);
 
