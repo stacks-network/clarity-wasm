6889a92af52600cb07509eb25111fa836627c02b
Anthony Caccia <anthony@bitcoinl2labs.com>
2024-01-19 14:29:31 +0100

    test(clar2wasm): add tests for constants declaration (thanks for writing them @csgui)

--- clar2wasm/src/words/constants.rs
+++ clar2wasm/src/words/constants.rs
@@ -60,3 +60,63 @@ impl ComplexWord for DefineConstant {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use clarity::vm::types::{ListData, ListTypeData, SequenceData};
+    use clarity::vm::Value;
+
+    use crate::tools::TestEnvironment;
+
+    #[test]
+    fn define_constant_const() {
+        let mut env = TestEnvironment::default();
+        let val = env.init_contract_with_snippet(
+            "define_constant",
+            r#"
+(define-constant four 4)
+(define-private (go) (print four))
+(go)
+"#,
+        );
+        assert_eq!(val.unwrap(), Some(Value::Int(4)));
+    }
+
+    #[test]
+    fn define_constant_function() {
+        let mut env = TestEnvironment::default();
+        let val = env.init_contract_with_snippet(
+            "define_constant",
+            r#"
+(define-constant four (+ 2 2))
+(define-private (go) (print four))
+(go)
+"#,
+        );
+        assert_eq!(val.unwrap(), Some(Value::Int(4)));
+    }
+
+    #[test]
+    fn define_constant_list() {
+        let mut env = TestEnvironment::default();
+        let val = env.init_contract_with_snippet(
+            "define_constant",
+            r#"
+(define-constant list-of-2-int (list 1 1))
+(define-private (go) (print list-of-2-int))
+(go)
+"#,
+        );
+        assert_eq!(
+            val.unwrap(),
+            Some(Value::Sequence(SequenceData::List(ListData {
+                data: vec![Value::Int(1), Value::Int(1)],
+                type_signature: ListTypeData::new_list(
+                    clarity::vm::types::TypeSignature::IntType,
+                    2
+                )
+                .unwrap()
+            })))
+        );
+    }
+}
