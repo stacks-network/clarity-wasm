0795c17f550f7820bca3abcbd0d1ffb439e1a674
Chris Guimaraes <cguimaraes@hiro.so>
2024-02-14 12:44:49 +0000

    tests: crossprop tests for comparison

--- clar2wasm/tests/wasm-generation/comparison.rs
+++ clar2wasm/tests/wasm-generation/comparison.rs
@@ -1,38 +1,11 @@
 use clar2wasm::tools::crosscheck_compare_only;
-use clarity::vm::types::{SequenceSubtype, TypeSignature};
-use clarity::vm::Value;
+use clarity::vm::types::{SequenceSubtype, StringSubtype, TypeSignature};
 use proptest::strategy::{Just, Strategy};
 use proptest::{prop_oneof, proptest};
 
-use crate::{string_ascii, PropValue};
+use crate::PropValue;
 
 const COMPARISONS_FUNC: [&str; 4] = ["<", "<=", ">", ">="];
-const INVALID_CHAR: char = '\'';
-
-fn filter_invalid_char(input: &Value) -> PropValue {
-    let v: String = input.clone().expect_ascii().replace(INVALID_CHAR, "");
-
-    PropValue::from(Value::from(clarity::vm::types::ASCIIData {
-        data: v.into(),
-    }))
-}
-
-proptest! {
-    #![proptest_config(super::runtime_config())]
-
-    #[test]
-    fn crossprop_comparison_string_ascii(val1 in string_ascii(50u32), val2 in string_ascii(50u32)) {
-
-        let v1 = filter_invalid_char(&val1);
-        let v2 = filter_invalid_char(&val2);
-
-        for func in &COMPARISONS_FUNC {
-            crosscheck_compare_only(
-                &format!("({func} {v1} {v2})")
-            )
-        }
-    }
-}
 
 fn strategies_for_comparison() -> impl Strategy<Value = TypeSignature> {
     prop_oneof![
@@ -40,6 +13,9 @@ fn strategies_for_comparison() -> impl Strategy<Value = TypeSignature> {
         Just(TypeSignature::UIntType),
         (0u32..128).prop_map(|s| TypeSignature::SequenceType(SequenceSubtype::BufferType(
             s.try_into().unwrap()
+        ))),
+        (0u32..128).prop_map(|s| TypeSignature::SequenceType(SequenceSubtype::StringType(
+            StringSubtype::ASCII(s.try_into().unwrap())
         )))
     ]
 }
