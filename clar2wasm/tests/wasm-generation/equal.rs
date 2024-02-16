1adc3103cc2dca2940ecc731e9d7b511df25d2a8
Chris Guimaraes <cguimaraes@hiro.so>
2024-02-16 21:09:29 +0000

    tests: add crossprop test for index-of

--- clar2wasm/tests/wasm-generation/equal.rs
+++ clar2wasm/tests/wasm-generation/equal.rs
@@ -1,4 +1,4 @@
-use clar2wasm::tools::crosscheck;
+use clar2wasm::tools::{crosscheck, crosscheck_compare_only};
 use proptest::proptest;
 
 use crate::PropValue;
@@ -36,5 +36,16 @@ proptest! {
             &format!(r#"(is-eq {val} {val} {val})"#),
             Ok(Some(clarity::vm::Value::Bool(true)))
         );
+    }
+}
+
+proptest! {
+    #![proptest_config(super::runtime_config())]
+
+    #[test]
+    fn crossprop_index_of(seq in PropValue::any_sequence(20usize), idx in (0..20u32)) {
+        crosscheck_compare_only(
+            &format!("(index-of? {seq} (try! (element-at? {seq} u{idx})))")
+        )
     }
 }
