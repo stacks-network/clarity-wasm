1982a51c07a54e38c729568fa1ea78a95c9d555b
Anthony Caccia <anthony@bitcoinl2labs.com>
2024-02-19 13:07:33 +0100

    test(tests): add the bns contract to ensure it works after issue 340 patch

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -2,3 +2,25 @@
 pub mod bin_tests;
 #[cfg(test)]
 pub mod lib_tests;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    /// Specific test for the fix of [issue 340](https://github.com/stacks-network/clarity-wasm/issues/340)
+    fn test_bns_contract_latest() {
+        let bns =
+            std::fs::read_to_string(env!("CARGO_MANIFEST_DIR").to_owned() + "/contracts/bns.clar")
+                .expect("Can't find bns contract");
+
+        // check for normal behavior with latest epoch/version
+        clar2wasm::tools::crosscheck(&bns, Ok(None));
+
+        // check with the issue's problematic epoch
+        assert!(clar2wasm::tools::evaluate_at(
+            &bns,
+            clarity::types::StacksEpochId::Epoch20,
+            clarity::vm::version::ClarityVersion::latest(),
+        )
+        .is_ok());
+    }
+}
