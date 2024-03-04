cc33b1b07cd927d1a6462e634eabf63e3bc8603d
Chris Guimaraes <cguimaraes@hiro.so>
2024-03-04 17:42:05 +0000

    tests: crossprop tests for noop functions

--- clar2wasm/tests/wasm-generation/noop.rs
+++ clar2wasm/tests/wasm-generation/noop.rs
@@ -2,7 +2,6 @@ use clar2wasm::tools::crosscheck;
 use clarity::vm::Value;
 use proptest::arbitrary::any;
 use proptest::proptest;
-use proptest::strategy::Strategy;
 
 use crate::PropValue;
 
@@ -10,13 +9,15 @@ proptest! {
     #![proptest_config(super::runtime_config())]
 
     #[test]
-    fn crosscheck_noop_to_uint(
-        val in any::<i128>()
-        .prop_filter("non-negative", |v| v >= &0i128)
-    ) {
+    fn crosscheck_noop_to_uint(val in any::<i128>()) {
+        let snippet = format!("(to-uint {})", PropValue(Value::Int(val)));
+
         crosscheck(
-            &format!("(to-uint {})", PropValue(Value::Int(val))),
-            Ok(Some(Value::UInt(val.try_into().unwrap())))
+            &snippet,
+            match val.try_into() {
+                Ok(v) => Ok(Some(Value::UInt(v))),
+                Err(_) => Err(())
+            }
         )
     }
 }
@@ -25,27 +26,15 @@ proptest! {
     #![proptest_config(super::runtime_config())]
 
     #[test]
-    fn crosscheck_noop_to_uint_err(
-        val in any::<i128>()
-        .prop_filter("negative", |v| v < &0i128)
-    ) {
-        crosscheck(
-            &format!("(to-uint {})", PropValue(Value::Int(val))),
-            Err(())
-        )
-    }
-}
+    fn crosscheck_noop_to_int(val in any::<u128>()) {
+        let snippet = format!("(to-int {})", PropValue(Value::UInt(val)));
 
-proptest! {
-    #![proptest_config(super::runtime_config())]
-
-    #[test]
-    fn crosscheck_noop_to_int(
-        val in any::<u128>()
-        .prop_filter("range", |v| v < &2u128.pow(126))) {
         crosscheck(
-            &format!("(to-int {})", PropValue(Value::UInt(val))),
-            Ok(Some(Value::Int(val.try_into().unwrap())))
+            &snippet,
+            match val.try_into() {
+                Ok(v) => Ok(Some(Value::Int(v))),
+                Err(_) => Err(())
+            }
         )
     }
 }
