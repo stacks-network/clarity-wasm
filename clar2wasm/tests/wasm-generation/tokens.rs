797ee87e3eef9e87491bbe8bc2ab7d560bc5ec0d
Anthony Caccia <anthony@bitcoinl2labs.com>
2024-05-02 10:46:20 +0200

    test(clar2wasm): proptest mint-transfer-getowner

--- clar2wasm/tests/wasm-generation/tokens.rs
+++ clar2wasm/tests/wasm-generation/tokens.rs
@@ -14,13 +14,13 @@ proptest! {
     #[test]
     fn nft_mint_get_owner(
         nft in PropValue::any(),
-        owner in PropValue::from_type(PrincipalType)
+        owner in PropValue::from_type(PrincipalType),
     ) {
         let snippet = format!(r#"
             (define-non-fungible-token stackaroo {})
             {{
                 mint: (nft-mint? stackaroo {nft} {owner}),
-                owner: (nft-get-owner? stackaroo {nft})
+                owner: (nft-get-owner? stackaroo {nft}),
             }}
         "#, nft.type_string());
 
@@ -34,6 +34,42 @@ proptest! {
                     ClarityName::from("owner"),
                     Value::some(owner.into()).unwrap(),
                 ),
+            ])
+            .unwrap(),
+        );
+
+        crosscheck(&snippet, Ok(Some(expected)));
+    }
+
+    #[test]
+    fn nft_mint_transfer_owner(
+        nft in PropValue::any(),
+        owner1 in PropValue::from_type(PrincipalType),
+        owner2 in PropValue::from_type(PrincipalType),
+    ) {
+        let snippet = format!(r#"
+            (define-non-fungible-token stackaroo {})
+            {{
+                a-mint: (nft-mint? stackaroo {nft} {owner1}),
+                b-transfer: (nft-transfer? stackaroo {nft} {owner1} {owner2}),
+                c-owner: (nft-get-owner? stackaroo {nft}),
+            }}
+        "#, nft.type_string());
+
+        let expected = Value::from(
+            TupleData::from_data(vec![
+                (
+                    ClarityName::from("a-mint"),
+                    Value::okay_true(),
+                ),
+                (
+                    ClarityName::from("b-transfer"),
+                    Value::okay_true(),
+                ),
+                (
+                    ClarityName::from("c-owner"),
+                    Value::some(owner2.into()).unwrap(),
+                )
             ])
             .unwrap(),
         );
