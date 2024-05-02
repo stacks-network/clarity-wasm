1b609c937bbbd6997b2f9f8fbbf36faa831893b5
Anthony Caccia <anthony@bitcoinl2labs.com>
2024-05-02 11:19:59 +0200

    test(clar2wasm): proptest for nft mint->burn->owner

--- clar2wasm/tests/wasm-generation/tokens.rs
+++ clar2wasm/tests/wasm-generation/tokens.rs
@@ -69,11 +69,47 @@ proptest! {
                 (
                     ClarityName::from("c-owner"),
                     Value::some(owner2.into()).unwrap(),
-                )
+                ),
             ])
             .unwrap(),
         );
 
         crosscheck(&snippet, Ok(Some(expected)));
     }
+
+    #[test]
+    fn nft_mint_burn_get_owner(
+        nft in PropValue::any(),
+        owner in PropValue::from_type(PrincipalType),
+    ) {
+        let snippet = format!(r#"
+            (define-non-fungible-token stackaroo {})
+            {{
+                a-mint: (nft-mint? stackaroo {nft} {owner}),
+                b-burn: (nft-burn? stackaroo {nft} {owner}),
+                c-owner: (nft-get-owner? stackaroo {nft}),
+            }}
+        "#, nft.type_string());
+
+        let expected = Value::from(
+            TupleData::from_data(vec![
+                (
+                    ClarityName::from("a-mint"),
+                    Value::okay_true(),
+                ),
+                (
+                    ClarityName::from("b-burn"),
+                    Value::okay_true(),
+                ),
+                (
+                    ClarityName::from("c-owner"),
+                    Value::none(),
+                ),
+            ])
+            .unwrap(),
+        );
+
+        crosscheck(&snippet, Ok(Some(expected)));
+    }
+
 }
