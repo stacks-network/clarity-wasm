5a8dc790f31bc768fac4c5ededb808f093f81261
Hugo Caillard <911307+hugocaillard@users.noreply.github.com>
2024-06-07 14:53:06 +0200

    refactor: improve wording in bns test

--- clar2wasm/tests/lib.rs
+++ clar2wasm/tests/lib.rs
@@ -7,23 +7,22 @@ pub mod lib_tests;
 mod tests {
     #[test]
     /// Specific test for the fix of [issue 340](https://github.com/stacks-network/clarity-wasm/issues/340)
-    fn test_bns_contract_latest() {
+    fn test_bns_contract_in_epoch2_4() {
         let bns = std::fs::read_to_string(
             env!("CARGO_MANIFEST_DIR").to_owned() + "/tests/contracts/bns.clar",
         )
         .expect("Can't find bns contract");
 
-        // check for normal behavior with latest epoch/version
-        clar2wasm::tools::crosscheck_with_epoch(
-            &bns,
-            Ok(None),
-            clarity::types::StacksEpochId::Epoch20,
-        );
-
-        // check with the issue's problematic epoch
         assert!(clar2wasm::tools::evaluate_at(
             &bns,
             clarity::types::StacksEpochId::Epoch20,
+            clarity::vm::version::ClarityVersion::Clarity1,
+        )
+        .is_ok());
+
+        assert!(clar2wasm::tools::evaluate_at(
+            &bns,
+            clarity::types::StacksEpochId::Epoch24,
             clarity::vm::version::ClarityVersion::Clarity2,
         )
         .is_ok());
