2aceb941da44da0808df393755c88618f2c1bdff
Ameer Hamza <ameeratwork@gmail.com>
2024-06-12 00:24:10 +0500

    prop-tests for (begin ...) added

--- clar2wasm/tests/wasm-generation/control_flow.rs
+++ clar2wasm/tests/wasm-generation/control_flow.rs
@@ -1,4 +1,6 @@
 use clar2wasm::tools::crosscheck;
+use clarity::vm::types::{OptionalData, ResponseData};
+use clarity::vm::Value;
 use proptest::proptest;
 
 use crate::PropValue;
@@ -59,6 +61,54 @@ proptest! {
         crosscheck(
             &format!(r#"(unwrap-err-panic (ok {val}))"#),
             Err(())
+        );
+    }
+}
+
+proptest! {
+    #![proptest_config(super::runtime_config())]
+
+    #[test]
+    fn begin(val in PropValue::any()) {
+        crosscheck(
+            &format!(r#"(begin (some {val}) {val})"#),
+            Ok(Some(val.into()))
+        );
+    }
+}
+
+proptest! {
+    #![proptest_config(super::runtime_config())]
+
+    #[test]
+    fn begin_ok(val in PropValue::any()) {
+        crosscheck(
+            &format!(r#"(begin (some {val}) (ok {val}))"#),
+            Ok(Some(Value::Response(ResponseData { committed: true, data: Box::new(val.into()) })))
+        );
+    }
+}
+
+proptest! {
+    #![proptest_config(super::runtime_config())]
+
+    #[test]
+    fn begin_err(val in PropValue::any()) {
+        crosscheck(
+            &format!(r#"(begin (some {val}) (err {val}))"#),
+            Ok(Some(Value::Response(ResponseData { committed: false, data: Box::new(val.into()) })))
+        );
+    }
+}
+
+proptest! {
+    #![proptest_config(super::runtime_config())]
+
+    #[test]
+    fn begin_some(val in PropValue::any()) {
+        crosscheck(
+            &format!(r#"(begin (some {val}))"#),
+            Ok(Some(Value::Optional(OptionalData { data: Some(Box::new(val.into())) })))
         );
     }
 }
