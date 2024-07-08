9fa1eb6864659dac3aeb64df78f23024fb09cc48
Chris Guimaraes <cguimaraes@hiro.so>
2024-07-08 12:46:35 +0100

    feat: wasm binary validation

--- clar2wasm/src/wasm_generator.rs
+++ clar2wasm/src/wasm_generator.rs
@@ -302,7 +302,21 @@ impl WasmGenerator {
             walrus::InitExpr::Value(walrus::ir::Value::I32(self.literal_memory_end as i32)),
         );
 
-        Ok(self.module)
+        if cfg!(debug_assertions) {
+            Ok(self.module)
+        // Validate Wasm binary in release mode
+        } else {
+            let wasm_buff = self.module.emit_wasm();
+            // Walrus::Module::from_buffer method is designed
+            // to parse and validate a Wasm binary.
+            // If the binary is valid, it returns an Ok(Module);
+            // otherwise, it returns an Err.
+            Module::from_buffer(&wasm_buff)
+                .map(|_| self.module)
+                .map_err(|_| {
+                    GeneratorError::InternalError("Wasm binary validation has failed".to_owned())
+                })
+        }
     }
 
     pub fn get_memory(&self) -> Result<MemoryId, GeneratorError> {
