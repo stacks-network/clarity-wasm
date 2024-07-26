7deda3fca315dc797247ad2ec5433ee6c8bb64e0
Anthony Caccia <anthony@bitcoinl2labs.com>
2024-07-26 14:39:03 +0200

    test: proptest for contract-call? using all arguments of a function

--- clar2wasm/tests/wasm-generation/contracts.rs
+++ clar2wasm/tests/wasm-generation/contracts.rs
@@ -1,5 +1,8 @@
 use clar2wasm::tools::crosscheck_multi_contract;
-use clarity::vm::{types::ResponseData, Value};
+use clarity::vm::{
+    types::{ResponseData, TupleData},
+    ClarityName, Value,
+};
 use proptest::prelude::*;
 
 use crate::{prop_signature, type_string, PropValue};
@@ -83,6 +86,66 @@ proptest! {
             Ok(Some(Value::Response(ResponseData {
                 committed: true,
                 data: Box::new(value.into()),
+            }))),
+        );
+    }
+
+    #[test]
+    fn contract_call_can_use_all_arguments(
+        (tys, values)
+            in prop::collection::vec(
+                prop_signature()
+                    .prop_ind_flat_map2(|ty| PropValue::from_type(ty.clone())),
+                1..=20
+            )
+            .prop_map(|arg_ty| arg_ty.into_iter().unzip::<_, _, Vec<_>, Vec<_>>())
+            .no_shrink(),
+    ) {
+        let first_contract_name = "foo".into();
+        let mut function_arguments = String::new();
+        for (name, ty) in ('a'..).zip(tys.iter()) {
+            write!(function_arguments, "({name} {}) ", type_string(ty)).unwrap();
+        }
+        let expected_res = ('a'..)
+            .take(tys.len())
+            .fold(String::new(), |mut output, arg| {
+                write!(output, "{arg}: {arg}, ").unwrap();
+                output
+            });
+        let first_snippet = format!(
+            r#"
+                (define-public (foofun {function_arguments})
+                    (ok {{ {expected_res} }})
+                )
+            "#
+        );
+
+        // second contract
+        let second_contract_name = "bar".into();
+        let mut call_arguments = String::new();
+        for value in values.iter() {
+            write!(call_arguments, "{value} ").unwrap();
+        }
+        let second_snippet =
+            format!(r#"(contract-call? .{first_contract_name} foofun {call_arguments})"#);
+
+        let expected = TupleData::from_data(
+            ('a'..)
+                .map(|c| ClarityName::try_from(c.to_string()).unwrap())
+                .zip(values.into_iter().map(Value::from))
+                .collect(),
+        )
+        .unwrap()
+        .into();
+
+        crosscheck_multi_contract(
+            &[
+                (first_contract_name, &first_snippet),
+                (second_contract_name, &second_snippet),
+            ],
+            Ok(Some(Value::Response(ResponseData {
+                committed: true,
+                data: Box::new(expected),
             }))),
         );
     }
