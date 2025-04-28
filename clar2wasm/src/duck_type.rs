c38df248a3aa6009142a4fedf313fb3aeb3df30c
Anthony Caccia <anthony@bitcoinl2labs.com>
2025-04-28 12:52:24 +0200

    test: add the platform to start testing duck-typing

--- clar2wasm/src/duck_type.rs
+++ clar2wasm/src/duck_type.rs
@@ -236,3 +236,198 @@ fn dt_needed_workspace(ty: &TypeSignature) -> u32 {
         _ => 0,
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use clarity::{
+        types::StacksEpochId,
+        vm::{
+            analysis::ContractAnalysis,
+            costs::LimitedCostTracker,
+            types::{QualifiedContractIdentifier, SequenceData, TypeSignature},
+            ClarityVersion, Value,
+        },
+    };
+    use walrus::{FunctionBuilder, InstrSeqBuilder};
+    use wasmtime::{Engine, Module, Store};
+
+    use crate::{
+        linker::dummy_linker,
+        wasm_generator::{
+            add_placeholder_for_clarity_type, clar2wasm_ty, GeneratorError, WasmGenerator,
+        },
+        wasm_utils::{placeholder_for_type, wasm_to_clarity_value},
+    };
+
+    impl WasmGenerator {
+        fn empty() -> Self {
+            let empty_analysis = ContractAnalysis::new(
+                QualifiedContractIdentifier::transient(),
+                vec![],
+                LimitedCostTracker::Free,
+                StacksEpochId::latest(),
+                ClarityVersion::latest(),
+            );
+            WasmGenerator::new(empty_analysis)
+                .expect("failed to build WasmGenerator for empty contract")
+        }
+
+        // TODO: this function shouldn't exist like this. As soon as #610 is done, we can
+        // replace it by a simple `write_to_wasm` call followed by `WasmGenerator::read_from_memory`.
+        fn pass_value(
+            &mut self,
+            builder: &mut InstrSeqBuilder,
+            value: &Value,
+            ty: &TypeSignature,
+        ) -> Result<(), GeneratorError> {
+            match value {
+                Value::Bool(b) => {
+                    builder.i32_const(*b as i32);
+                    Ok(())
+                }
+                Value::Int(i) => {
+                    builder.i64_const((i & 0xFFFFFFFFFFFFFFFF) as i64);
+                    builder.i64_const(((i >> 64) & 0xFFFFFFFFFFFFFFFF) as i64);
+                    Ok(())
+                }
+                Value::UInt(u) => {
+                    builder.i64_const((u & 0xFFFFFFFFFFFFFFFF) as i64);
+                    builder.i64_const(((u >> 64) & 0xFFFFFFFFFFFFFFFF) as i64);
+                    Ok(())
+                }
+                Value::Sequence(SequenceData::String(s)) => {
+                    let (offset, len) = self.add_clarity_string_literal(s)?;
+                    builder.i32_const(offset as i32);
+                    builder.i32_const(len as i32);
+                    Ok(())
+                }
+                Value::Principal(_) | Value::Sequence(SequenceData::Buffer(_)) => {
+                    let (offset, len) = self.add_literal(value)?;
+                    builder.i32_const(offset as i32);
+                    builder.i32_const(len as i32);
+                    Ok(())
+                }
+                Value::Optional(opt) => {
+                    let TypeSignature::OptionalType(inner_ty) = ty else {
+                        return Err(GeneratorError::InternalError(
+                            "Mismatched value/type".to_owned(),
+                        ));
+                    };
+                    match &opt.data {
+                        Some(inner) => {
+                            builder.i32_const(1);
+                            self.pass_value(builder, inner, inner_ty)?;
+                        }
+                        None => {
+                            builder.i32_const(0);
+                            add_placeholder_for_clarity_type(builder, inner_ty);
+                        }
+                    }
+                    Ok(())
+                }
+                Value::Response(resp) => {
+                    let TypeSignature::ResponseType(resp_ty) = ty else {
+                        return Err(GeneratorError::InternalError(
+                            "Mismatched value/type".to_owned(),
+                        ));
+                    };
+                    builder.i32_const(resp.committed as i32);
+                    if resp.committed {
+                        self.pass_value(builder, &resp.data, &resp_ty.0)?;
+                        add_placeholder_for_clarity_type(builder, &resp_ty.1);
+                    } else {
+                        add_placeholder_for_clarity_type(builder, &resp_ty.0);
+                        self.pass_value(builder, &resp.data, &resp_ty.1)?;
+                    }
+                    Ok(())
+                }
+                Value::Tuple(tuple) => {
+                    let TypeSignature::TupleType(tuple_ty) = ty else {
+                        return Err(GeneratorError::InternalError(
+                            "Mismatched value/type".to_owned(),
+                        ));
+                    };
+                    for (elem, elem_ty) in tuple
+                        .data_map
+                        .values()
+                        .zip(tuple_ty.get_type_map().values())
+                    {
+                        self.pass_value(builder, elem, elem_ty)?;
+                    }
+                    Ok(())
+                }
+                Value::Sequence(SequenceData::List(_list)) => {
+                    todo!("Need to complete #610 first")
+                }
+                Value::CallableContract(_) => unimplemented!("We can already test principals"),
+            }
+        }
+
+        fn create_duck_type_module(
+            &mut self,
+            value: &Value,
+            original_type: &TypeSignature,
+            target_type: &TypeSignature,
+        ) {
+            let return_ty = clar2wasm_ty(target_type);
+            let mut top_level = FunctionBuilder::new(&mut self.module.types, &[], &return_ty);
+            {
+                let builder = &mut top_level.func_body();
+
+                self.pass_value(builder, value, original_type)
+                    .expect("failed to write instructions for original value");
+
+                self.duck_type(builder, original_type, target_type)
+                    .expect("failed to write duck type instructions");
+            }
+
+            let top_level = top_level.finish(vec![], &mut self.module.funcs);
+            self.module.exports.add(".top-level", top_level);
+
+            self.module.globals.get_mut(self.stack_pointer).kind =
+                walrus::GlobalKind::Local(walrus::InitExpr::Value(walrus::ir::Value::I32(20000)));
+        }
+
+        fn execute_duck_type_module(&mut self, target_type: &TypeSignature) -> Value {
+            std::fs::write("debug.wasm", self.module.emit_wasm()).unwrap();
+
+            let engine = Engine::default();
+            let mut store = Store::new(&engine, ());
+            let linker = dummy_linker(&engine).expect("failed to create linker");
+            let module =
+                Module::new(&engine, self.module.emit_wasm()).expect("failed to create module");
+            let instance = linker
+                .instantiate(&mut store, &module)
+                .expect("failed to instanciate module");
+
+            let top_level = instance
+                .get_func(&mut store, ".top-level")
+                .expect("cannot find .top-level function");
+
+            let mut result: Vec<_> = top_level
+                .ty(&mut store)
+                .results()
+                .map(placeholder_for_type)
+                .collect();
+
+            top_level
+                .call(&mut store, &[], &mut result)
+                .expect("couldn't call .top-level");
+
+            let memory = instance
+                .get_memory(&mut store, "memory")
+                .expect("couldn't find memory");
+            wasm_to_clarity_value(
+                target_type,
+                0,
+                &result,
+                memory,
+                &mut store,
+                StacksEpochId::latest(),
+            )
+            .expect("error in execution")
+            .0
+            .expect("no value computed???")
+        }
+    }
+}
