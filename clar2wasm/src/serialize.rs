5b0ae3f3d73d3bf0e50a7bae2bee845a9c6eb18b
Anthony Caccia <anthony@bitcoinl2labs.com>
2025-09-05 23:29:24 +0200

    test: create the method to test the serialization size of a value

--- clar2wasm/src/serialize.rs
+++ clar2wasm/src/serialize.rs
@@ -1254,3 +1254,53 @@ impl WasmGenerator {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use clarity::vm::{
+        types::{TupleData, TupleTypeSignature, TypeSignature},
+        Value,
+    };
+
+    use crate::wasm_generator::WasmGenerator;
+
+    fn test_serialization_size(value: Value, ty: TypeSignature) {
+        let mut gen = WasmGenerator::empty();
+
+        // since `serialization_size` push on the stack the value and the size,
+        // we'll expect a tuple {a: value, b: size}
+        let return_ty: TypeSignature = TupleTypeSignature::try_from(vec![
+            ("a".into(), ty.clone()),
+            ("b".into(), TypeSignature::UIntType),
+        ])
+        .unwrap()
+        .into();
+
+        gen.create_module(&return_ty, |gen, builder| {
+            gen.pass_value(builder, &value, &ty)
+                .expect("failed to write instructions for original value");
+
+            gen.serialization_size(builder, &ty)
+                .expect("failed to write serialization size instructions");
+
+            // we need to extend the u32 we get for the size into a uint
+            builder
+                .unop(walrus::ir::UnaryOp::I64ExtendUI32)
+                .i64_const(0);
+        });
+
+        let res = gen.execute_module(&return_ty);
+
+        let expected_size = value
+            .serialized_size()
+            .expect("could not compute serialized size");
+        let expected = TupleData::from_data(vec![
+            ("a".into(), value),
+            ("b".into(), Value::UInt(expected_size as u128)),
+        ])
+        .unwrap()
+        .into();
+
+        assert_eq!(res, expected);
+    }
+}
